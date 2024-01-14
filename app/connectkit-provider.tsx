13e47df87f913043515f553cf18e5d0110d8769d
Aryan Malik <aryanhunny98@gmail.com>
2024-01-14 23:07:35 +0530

    Recreated page.tsx

---
+++ app/connectkit-provider.tsx
@@ -0,0 +1,27 @@
+import { WagmiConfig, createConfig } from 'wagmi';
+import { ConnectKitProvider, ConnectKitButton, getDefaultConfig } from 'connectkit';
+
+const config = createConfig(
+  getDefaultConfig({
+    // API keys
+    alchemyId: process.env.INFURA_ID,
+    walletConnectProjectId: process.env.WALLETCONNECT_PROJECT_ID || '',
+
+    // App info
+    appName: 'ConnectKit Demo',
+    appDescription: 'ConnectKit Demo App',
+    appUrl: 'https://family.co',
+    appIcon: 'https://family.co/logo.png',
+  }),
+);
+
+export const ConnectkitProvider = ({ children }: any) => {
+  return (
+    <WagmiConfig config={config}>
+      <ConnectKitProvider>
+        {children}
+        <ConnectKitButton />
+      </ConnectKitProvider>
+    </WagmiConfig>
+  );
+}
